035b6aed73e992977678433827ad0bb92b57e0b8
Reenuhaa Pechimuthu Babu Seethalakshmi <107914703+reenuhaa@users.noreply.github.com>
2026-09-22 14:51:41 -0500

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+   string encode(vector<string>& strs) {
+        ostringstream ss;
+        for(auto& i:strs){
+            ss << i.length()<< '#' << i;
+        }
+        cout<<"encoded str:"<<ss.str()<<endl;
+        return ss.str();
+    }
+
+    vector<string> decode(string s) {
+        istringstream ss{s};
+        int len;
+        char delimiter{};
+
+        std::vector<string> strings;
+        while(ss >> len >> delimiter)
+        {
+            //cout<<"len:"<<len<<" delimiter:"<<delimiter<<endl;
+
+            std::string s;
+            s.resize(len);
+
+            int i{0};
+            while(i < len){
+                s[i++] = ss.get();
+            }
+            
+            //cout<<"reconstructed string:"<<s<<endl;
+            
+            strings.push_back(std::move(s));
+        }
+
+        return strings;
+    }
+};
