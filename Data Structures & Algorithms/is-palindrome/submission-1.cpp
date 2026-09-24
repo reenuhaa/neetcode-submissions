9267cd1a7dc52401bad9899f5e5aacba11ca8bca
Reenuhaa Pechimuthu Babu Seethalakshmi <107914703+reenuhaa@users.noreply.github.com>
2026-09-24 10:40:53 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        cout<<"s.length():"<<s.length()<<endl;
+
+        //O(N)
+        auto newEnd = std::remove_if(s.begin(),s.end(),[](char ch){
+            if(!((ch >=48 && ch<=57)||(ch >=65 && ch<=90)||(ch >=97 && ch<=122))){
+                cout<<"ignoring ch:"<<std::boolalpha<<ch<<endl;
+                return true;
+            }
+            return false;
+        });
+
+        auto newLen = std::distance(s.begin(),newEnd);
+        cout<<"newLen:"<<newLen<<endl;
+        int j = newLen-1;
+        
+        //O(N/2)
+        for(int i=0; i< newLen/2 ;){
+            if(tolower(s[i++]) != tolower(s[j--])){
+                return false;
+            }
+        }
+        return true;
+    } //TC: O(N/2 + N)=> O(3/2N)==> O(N) ; SC:O(1)
+};
